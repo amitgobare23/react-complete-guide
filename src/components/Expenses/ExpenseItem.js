718aa5df5eddcaeb98bad3378cf806b22d94c522
Amit Gobare <amitgobare23@gmail.com>
2022-03-25 22:54:13 +0530

    Section 3-React basics and working with components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
+import React from 'react';
 import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from '../Expenses/ExpenseDate';
+import Card from '../UI/Card';
 
-function ExpenseItem(props){
+const ExpenseItem = (props) => {
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 
 }
